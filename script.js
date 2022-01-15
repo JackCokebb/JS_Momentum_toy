b740c51d5faeb61e94ab6a57b97b5241c4148e68
JackCokebb <reo2726@gmail.com>
2022-01-15 16:37:51 +0900

    js test

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hi");
+console.log(2423);
+console.log("sdasd");
+
+const x = 5;
+const y = 4;
+const myName = "JEE";
+//x = 6;
+console.log(x);
+console.log(x + y);
+console.log("hello " + myName);
