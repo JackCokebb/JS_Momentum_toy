fd20e3f126ef1886233fc9ab1040d4e9cb2ee75c
JackCokebb <reo2726@gmail.com>
2022-01-19 19:34:59 +0900

    js test

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log(2423);
 console.log("sdasd");
 
-const x = 5;
-const y = 4;
+let z = 21; //can be updated
+const x = 5; // value can not be updated
+var y = 4; //we are not protected by language
 const myName = "JEE";
 //x = 6;
 console.log(x);
 console.log(x + y);
 console.log("hello " + myName);
+alert("hi");
