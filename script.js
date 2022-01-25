64d296cf1abe6d140235d8ab54b0b115b659f905
JackCokebb <reo2726@gmail.com>
2022-01-25 14:57:26 +0900

    js test

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ var y = 4; //we are not protected by language
 const empty = null;
 const myName = "JEE";
 //x = 6;
-console.log(x);
-console.log(x + y);
-console.log("hello " + myName);
+//console.log(x);
+//console.log(x + y);
+//console.log("hello " + myName);
 //alert("hi");
 const mon = "mon";
 const array = [1, 2, 3, "mon", mon];
-console.log(array);
+//console.log(array);
+array.push("sun");
+//console.log(array);
+
+//creating object
+const player = {
+  name: "JEE",
+  age: 26,
+  money: 300,
+};
+console.log(player);
+//console.log(player["name"]);
+player.money = 4000;
+//console.log(player.money);
+player.height = 181;
+console.log(player);
