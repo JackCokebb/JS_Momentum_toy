4d69ad175f8ec8bbffb69132e30e115dbd3c4bb0
JackCokebb <reo2726@gmail.com>
2022-01-26 15:30:24 +0900

    js test with html

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-//alert(document.title);
-const title = document.getElementById("title");
-console.dir(title);
-alert("title will be changed");
-title.innerText = "Got you";
+function handleTitleClicked() {
+  console.log("title was clicked!");
+}
+
+const hellos = document.getElementsByClassName("hello");
+//console.log(hellos);
+const got = document.getElementsByTagName("h1");
+//console.log(got);
+const gott = document.querySelector(".hello:first-child"); //css selector
+console.dir(gott);
+//gott.style.color = "blue";
+gott.addEventListener("click", handleTitleClicked);
