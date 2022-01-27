f40686a2e3b59383e117ef4e6d62c9fdf48ae097
JackCokebb <reo2726@gmail.com>
2022-01-27 10:59:33 +0900

    js test event&style

--- test.js
+++ test.js
@@ -50,3 +50,53 @@ if (isNaN(userAge)) {
 }
 
 console.log(typeof userAge);
+
+/*event listener + style*/
+let click_count = 0;
+
+function handleTitleClicked() {
+  console.log("title was clicked!");
+  //gott.style.color = "blue";
+  click_count++;
+  if (click_count > 5) {
+    gott.innerText = "STOP!";
+    gott.style.color = "red";
+  } else if (gott.style.color === "blue") {
+    gott.style.color = "tomato";
+  } else {
+    gott.style.color = "blue";
+  }
+}
+
+function handleTitleEntered() {
+  console.log("title was entered");
+  got2.style.color = "green";
+}
+function handleTitleLeft() {
+  console.log("mouse left");
+  got2.style.color = "black";
+}
+function handleWindowResize() {
+  console.log("window resized");
+  document.body.style.backgroundColor = "tomato";
+}
+function handleCopy() {
+  alert("copier!");
+}
+
+const hellos = document.getElementsByClassName("hello");
+//console.log(hellos);
+const got = document.getElementsByTagName("h1");
+//console.log(got);
+const gott = document.querySelector(".hello:first-child"); //css selector
+console.dir(gott);
+//gott.style.color = "blue";
+gott.addEventListener("click", handleTitleClicked);
+
+const got2 = document.querySelector("h2");
+//got2.addEventListener("mouseenter", handleTitleEntered);
+got2.onmouseenter = handleTitleEntered;
+got2.addEventListener("mouseleave", handleTitleLeft);
+
+//window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleCopy);
