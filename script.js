5c917bf796e60a2e94bf9adc0bf17ab91806f918
JackCokebb <reo2726@gmail.com>
2022-01-28 15:08:22 +0900

    page building

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-function handleLoginButtonClick() {
-  console.dir(loginInput);
-  console.log("login clicked");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "userName";
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
   const userName = loginInput.value;
-  console.log("hello! " + userName);
+  localStorage.setItem(USERNAME_KEY, userName);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  //greeting.innerText = "Hello " + userName;
+  greeting.innerText = `Hello ${userName}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", handleLoginButtonClick);
+loginForm.addEventListener("submit", handleLoginSubmit);
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if (savedUserName === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+  //show the greeting
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${savedUserName}!`;
+}
