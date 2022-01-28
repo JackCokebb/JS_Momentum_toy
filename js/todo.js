c67e57c67d2816074371917e3a83d7a15f5519d5
JackCokebb <reo2726@gmail.com>
2022-01-28 22:36:59 +0900

    page building making todo list

--- js/todo.js
+++ js/todo.js
@@ -2,19 +2,38 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todo", JSON.stringify(toDos)); //array to string
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
+  const cancelButton = document.createElement("button");
   span.innerText = newTodo;
+  cancelButton.innerText = "❌";
+  cancelButton.addEventListener("click", onCancelClicked);
+
+  li.appendChild(span);
+  li.appendChild(cancelButton);
+
   todoList.appendChild(li);
 }
 
+function onCancelClicked(event) {
+  //delete todo
+  const target_li = event.target.parentElement;
+  target_li.remove();
+}
 function handleToDosubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = ""; // to make todo box empty
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 todoForm.addEventListener("submit", handleToDosubmit);
